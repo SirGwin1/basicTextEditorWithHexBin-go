d62cbb5cb47a9ebc8dd84b9c94f6db11fe4eff6b
SirGwin1 <akorgoodwin01@gmail.com>
2026-03-25 09:28:12 +0100

    numberBaseModifier.go; a function that helps convert from hex and bin to decimal

--- functionality/numberBaseModifier.go
+++ functionality/numberBaseModifier.go
@@ -1,5 +1,30 @@
-package functionalities
+package functionality
 
-func numberBaseModifier(str string) string {
+import (
+	"strconv"
+	"strings"
+)
 
+func BaseConversion(str string) string {
+	sli := strings.Fields(str)
+
+	for i := 0; i < len(sli); i++ {
+		if sli[i] == "(hex)" && i > 0 {
+			result, err := strconv.ParseInt(sli[i-1], 16, 64)
+			if err == nil {
+				sli[i-1] = strconv.Itoa(int(result))
+				sli = append(sli[:i], sli[i+1:]...)
+				i-- // adjust index after removal
+			}
+		} else if sli[i] == "(bin)" && i > 0 {
+			result, err := strconv.ParseInt(sli[i-1], 2, 64)
+			if err == nil {
+				sli[i-1] = strconv.Itoa(int(result))
+				sli = append(sli[:i], sli[i+1:]...)
+				i-- // adjust index after removal
+			}
+		}
+	}
+
+	return strings.Join(sli, " ")
 }
