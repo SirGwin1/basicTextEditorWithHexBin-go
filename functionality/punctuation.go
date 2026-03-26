827fd685f2d47487a1222b23c9c4fbabd25c7c1e
gakor <akorgodwin01@gmail.com>
2026-03-26 20:15:10 +0100

    this is a function to check and edit the punctuation in a text file

--- functionality/punctuation.go
+++ functionality/punctuation.go
@@ -1,5 +1,19 @@
-package functionalities
+package functionality
 
-func punctuation(str string) string {
+import "regexp"
 
+func Punctuation(str string) string {
+	// Remove spaces before punctuation
+	reBefore := regexp.MustCompile(`\s+([,.;:!?])`)
+	str = reBefore.ReplaceAllString(str, `$1`)
+
+	// Ensure single space after punctuation
+	reAfter := regexp.MustCompile(`([,.;:!?])([^\s])`)
+	str = reAfter.ReplaceAllString(str, `$1 $2`)
+
+	// Fix spaces around quotes
+	reQuote := regexp.MustCompile(`\s*(['"])\s*`)
+	str = reQuote.ReplaceAllString(str, `$1`)
+
+	return str
 }
