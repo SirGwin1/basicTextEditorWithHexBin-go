9014210cedc89e0a2c39cd32e24b05077580ed1e
gakor <akorgodwin01@gmail.com>
2026-04-15 05:47:42 +0100

    the first function in the caseModifier.go file

--- functionality/caseModifier.go
+++ functionality/caseModifier.go
@@ -1,5 +1,30 @@
-package functionalities
+package functionality
 
-func caseModifier(str string) string {
+import (
+	"strconv"
+	"strings"
+)
 
+func CaseModifier(s string) string {
+
+	words := strings.Split(s, " ")
+	lastWord := words[len(words)-1]
+	lastWord = strings.Trim(lastWord, "()")
+
+	parts := strings.Split(lastWord, ",")
+
+	if len(parts) != 2 || parts[0] != "up" {
+		return s
+	}
+	n, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return s
+	}
+	words = words[:len(words)-1]
+	for i := len(words) - n; i < len(words); i++ {
+		if i >= 0 {
+			words[i] = strings.ToUpper(words[i])
+		}
+	}
+	return strings.Join(words, " ")
 }
